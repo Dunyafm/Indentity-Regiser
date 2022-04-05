20e547bcbe3021b7cd9ba0f808b30cc1e4ea41e9
Dunya Mammadli <dunyamamadli@gmail.com>
2022-04-06 03:30:49 +0400

    AccountController,ValidationPartial,RegisterVm changed

---
+++ LessonMigration/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using LessonMigration.Models;
+using LessonMigration.ViewModels.Account;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonMigration.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+
+            if (!ModelState.IsValid) return View(registerVM);
+            AppUser newuser = new AppUser()
+            {
+                FullName = registerVM.FullName,
+                UserName = registerVM.UserName,
+                Email = registerVM.Email
+
+            };
+            var result = await _userManager.CreateAsync(newuser, registerVM.Password);
+            if(!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                    return View(registerVM);
+
+                }
+            }
+            return View();
+        }
+
+
+
+    }
+  
+    
+}
